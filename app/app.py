32b66d70053d84269ac9807e13ad98948b6d60d7
Vishnu Varma <vishnuvarma234@gmail.com>
2026-07-21 15:55:49 +0530

    Update application version

--- app/app.py
+++ app/app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Employee DevOps Project is Running!"
-
+    return "🚀 Version 2 - CI/CD Pipeline Working Successfully!"
 @app.route("/health")
 def health():
     return jsonify({
